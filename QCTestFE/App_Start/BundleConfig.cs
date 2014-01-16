971dac23a483db960a800546eae4bcaa911b1262
DigiBanks99 <wlboshoff@gmail.com>
2014-01-16 16:50:29 +0200

    Added Deployment settings.

--- QCTestFE/App_Start/BundleConfig.cs
+++ QCTestFE/App_Start/BundleConfig.cs
@@ -8,18 +8,22 @@ namespace QCTestFE
     // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
     public static void RegisterBundles(BundleCollection bundles)
     {
-      bundles.Add(new StyleBundle("~/content/css/app").Include("~/content/app.min.css"));
+      bundles.Add(new StyleBundle("~/content/css").Include(
+        "~/content/app.css",
+        "~/content/header.css",
+        "~/content/accordian.css",
+        "~/content/shopping.css"));
 
       bundles.Add(new ScriptBundle("~/js/jquery").Include("~/scripts/vendor/jquery-{version}.js"));
 
       bundles.Add(new ScriptBundle("~/js/app").Include(
-          "~/scripts/vendor/angular.js",
-          "~/scripts/vendor/angular-route.js",
-          "~/scripts/vendor/angular-sanitize.js",
-          "~/scripts/app.js",
-          "~/scripts/controllers.js",
-          "~/scripts/accordion.js",
-          "~/scripts/directives/directives.js"));
+        "~/scripts/vendor/angular.js",
+        "~/scripts/vendor/angular-route.js",
+        "~/scripts/vendor/angular-sanitize.js",
+        "~/scripts/app.js",
+        "~/scripts/controllers.js",
+        "~/scripts/accordion.js",
+        "~/scripts/directives/directives.js"));
     }
   }
 }
